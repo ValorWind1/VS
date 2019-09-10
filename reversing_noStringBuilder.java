d25a9d7a19d96a6481c3ed711984a6db3c7e7b21
ValorWind1 <skyjdani@gmail.com>
2019-09-09 23:27:36 -0400

    1.3 reverse String no StringBuilder Example

---
+++ reversing_noStringBuilder.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class reversing_noStringBuilder {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Type your String : ");
+
+        String string1;
+
+        string1 = sc.nextLine();
+
+        String[] array1 = string1.split("");
+
+        for(int i=array1.length-1;i>=0;i--){
+            
+            System.out.print(array1[i]+"");
+            
+        }
+        System.out.println();
+
+
+    }
+}
--- testVS.java
+++ testVS.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 
 public class testVS{
     public static void main(String[] args) {
        
-        System.out.println("Hello World ");
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type your string : ");
+
+        String string1 ; 
+
+        string1 = sc.nextLine();
+
+        String [] array1 = string1.split("");
+
+        for(int i = array1.length-1;i>=0;i--){
+            System.out.print(array1[i]);
+        }
+        System.out.println();
+       
+ }
 }
