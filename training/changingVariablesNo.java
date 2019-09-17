18465be8ffd359d73c21456e19f64229f6349f9c
ValorWind1 <skyjdani@gmail.com>
2019-09-16 21:57:28 -0400

    static variable demo

---
+++ training/changingVariablesNo.java
@@ -0,0 +1,19 @@
+public class changingVariablesNo {
+
+    public static void main(String[] args) {
+        
+        // x = 30 
+        // y = 10 
+
+        int x = 10 ; 
+        int y = 20 ; 
+
+        y = y - x ; 
+
+        System.out.println(y);
+
+        x = x + y ; 
+
+        System.out.println(x);
+    }
+}
---
+++ training/staticVariableDemo.java
@@ -0,0 +1,11 @@
+ public class staticVariableDemo { 
+    // static variable 
+    static int a = 1; 
+            
+    // our main method is also static * 
+    public static void main(String[] args) {
+       System.out.println("Value of a : "+a); 
+       System.out.println("from main"); 
+    } 
+  
+} 
